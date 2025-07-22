46152c4690e8bbe6ce4aaf4bf720ab846f9a992d
Yhumi <yhumi@miyei.me>
2025-07-22 22:50:24 +0100

    Re-enable elytra/rocket interaction but increase durability cost.

--- src/main/java/yhumi/losercraft/mixin/FireworkRocketEntityMixin.java
+++ src/main/java/yhumi/losercraft/mixin/FireworkRocketEntityMixin.java
@@ -1,24 +1,49 @@
 package yhumi.losercraft.mixin;
 
+import java.util.List;
+
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.Util;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.FireworkRocketEntity;
+import net.minecraft.world.item.ItemStack;
 
 @Mixin(FireworkRocketEntity.class)
 public class FireworkRocketEntityMixin {
+    @Shadow private LivingEntity attachedToEntity;
+
     @Inject(
         method = "tick()V",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/world/entity/LivingEntity;isFallFlying()Z",
+            target = "Lnet/minecraft/world/entity/LivingEntity;getHandHoldingItemAngle(Lnet/minecraft/world/item/Item;)Lnet/minecraft/world/phys/Vec3;",
             shift = At.Shift.BEFORE
         ),
         cancellable = true
     )
-    private void disableElytraRocketBoost(CallbackInfo ci) {
-        ci.cancel();
+    protected void reduceElytraDurabilityExtra(CallbackInfo ci) {
+        List<EquipmentSlot> list = EquipmentSlot.VALUES
+            .stream()
+            .filter(equipmentSlotx -> LivingEntity.canGlideUsing(this.attachedToEntity.getItemBySlot(equipmentSlotx), equipmentSlotx))
+            .toList();
+        
+        if (list.isEmpty()) {
+            return;
+        }
+
+        EquipmentSlot equipmentSlot = Util.getRandom(list, this.attachedToEntity.getRandom());
+        ItemStack itemInSlot = this.attachedToEntity.getItemBySlot(equipmentSlot);
+
+        int itemDurability = itemInSlot.getMaxDamage() - itemInSlot.getDamageValue();
+        if (itemDurability > 1) {
+            int tickDurabilityReduction = Math.min((itemDurability) - 1, 4);
+            itemInSlot.hurtAndBreak(tickDurabilityReduction, this.attachedToEntity, equipmentSlot);
+        }
     }
 }
--- src/main/java/yhumi/losercraft/mixin/FireworkRocketItemMixin.java
+++ src/main/java/yhumi/losercraft/mixin/FireworkRocketItemMixin.java
@@ -1,25 +1,25 @@
-package yhumi.losercraft.mixin;
+// package yhumi.losercraft.mixin;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+// import org.spongepowered.asm.mixin.Mixin;
+// import org.spongepowered.asm.mixin.injection.At;
+// import org.spongepowered.asm.mixin.injection.Inject;
+// import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.item.FireworkRocketItem;
+// import net.minecraft.world.InteractionResult;
+// import net.minecraft.world.item.FireworkRocketItem;
 
-@Mixin(FireworkRocketItem.class)
-public class FireworkRocketItemMixin {
-    @Inject(
-        method = "use(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResult;",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/world/entity/player/Player;getItemInHand(Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/item/ItemStack;",
-            shift = At.Shift.BEFORE
-        ),
-        cancellable = true
-    )
-    private void cancelElytraFlightRocketBoost(CallbackInfoReturnable<InteractionResult> infoReturnable) {
-        infoReturnable.setReturnValue(InteractionResult.FAIL);
-    }
-}
+// @Mixin(FireworkRocketItem.class)
+// public class FireworkRocketItemMixin {
+//     @Inject(
+//         method = "use(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResult;",
+//         at = @At(
+//             value = "INVOKE",
+//             target = "Lnet/minecraft/world/entity/player/Player;getItemInHand(Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/item/ItemStack;",
+//             shift = At.Shift.BEFORE
+//         ),
+//         cancellable = true
+//     )
+//     private void cancelElytraFlightRocketBoost(CallbackInfoReturnable<InteractionResult> infoReturnable) {
+//         //infoReturnable.setReturnValue(InteractionResult.FAIL);
+//     }
+// }
